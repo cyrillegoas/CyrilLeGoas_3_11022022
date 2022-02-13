e30347f9d0c37c9f94752b84db003e6554ef2b67
Cyril Le Goas <cyril.legoas@gmail.com>
2022-02-13 17:24:43 +0100

    ADD palette du gout restaurant page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+const { resolve } = require("path");
 
 export default defineConfig({
   base: "./",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "restaurants/palette_du_gout.html"),
+      },
+    },
   },
 });
