d4a9ecf05d79705419784b33fd455cb221b4f5e1
Cyril Le Goas <cyril.legoas@gmail.com>
2022-02-15 22:59:58 +0100

    ADD new restaurants

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         nested: resolve(__dirname, "restaurants/palette_du_gout.html"),
+        nested: resolve(__dirname, "restaurants/delice_des_sens.html"),
+        nested: resolve(__dirname, "restaurants/a_la_francaise.html"),
+        nested: resolve(__dirname, "restaurants/note_enchantee.html"),
       },
     },
   },
