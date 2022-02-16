ad5c607e2f9064ef77181fdce6c3462abadecf84
Cyril Le Goas <cyril.legoas@gmail.com>
2022-02-16 16:04:25 +0100

    FIX vite not bundling restaurant pages

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "restaurants/palette_du_gout.html"),
-        nested: resolve(__dirname, "restaurants/delice_des_sens.html"),
-        nested: resolve(__dirname, "restaurants/a_la_francaise.html"),
-        nested: resolve(__dirname, "restaurants/note_enchantee.html"),
+        nested1: resolve(__dirname, "restaurants/a_la_francaise.html"),
+        nested2: resolve(__dirname, "restaurants/delice_des_sens.html"),
+        nested3: resolve(__dirname, "restaurants/note_enchantee.html"),
+        nested4: resolve(__dirname, "restaurants/palette_du_gout.html"),
       },
     },
   },
