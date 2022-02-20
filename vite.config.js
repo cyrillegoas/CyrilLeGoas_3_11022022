f5c0fabe836030f8895c84482f0691e660fb2a61
Cyril Le Goas <cyril.legoas@gmail.com>
2022-02-20 22:05:18 +0100

    FIX vite config wrong path / file name

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from "vite";
 const { resolve } = require("path");
 
 export default defineConfig({
-  base: "./",
   build: {
-    outDir: "dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested1: resolve(__dirname, "restaurants/a_la_francaise.html"),
-        nested2: resolve(__dirname, "restaurants/delice_des_sens.html"),
-        nested3: resolve(__dirname, "restaurants/note_enchantee.html"),
-        nested4: resolve(__dirname, "restaurants/palette_du_gout.html"),
+        nested1: resolve("./restaurants/", "a_la_francaise.html"),
+        nested2: resolve("./restaurants/", "delice_des_sens.html"),
+        nested3: resolve("./restaurants/", "note_enchantee.html"),
+        nested4: resolve("./restaurants/", "palette_du_gout.html"),
       },
     },
   },
